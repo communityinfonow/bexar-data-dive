f3edeb5e7a26db41eca2a0b105cc419f58bba768
Brian <herronrb@gvsu.edu>
2022-06-03 13:26:43 -0400

    responsive chart

--- src/frontend/src/components/MyCommunityChart.vue
+++ src/frontend/src/components/MyCommunityChart.vue
@@ -43,57 +43,61 @@ export default {
 	mounted () {
 		echarts.use([SVGRenderer, LegendComponent, GridComponent, BarChart]);
 		this.chart = echarts.init(this.$refs['chart_container_' + this.indicatorId], null, { renderer: 'svg'});
+		window.addEventListener('resize', () => {
+			this.chart.resize();
+		});
 		this.drawChart();
 	},
 	methods: {
 		drawChart() {
 			let option = {};
-		if (this.data[0].baseFilter) {
-			option.legend = { data: this.data.map(d => d.baseFilter['name_' + this.locale])};
-		}
-		option.yAxis = { 
-			type: 'value', 
-			splitLine: { show: false }
-		};
-		option.xAxis = { 
-			type: 'category', 
-			data: Array.from(new Set(this.data.map(d => d.raceFilter['name_' + this.locale]))),
-			axisTick: { show: false }
-		};
-		let filters = new Set(this.data.map(d => d.baseFilter?.id || 0))
-		option.color = colorbrewer.Blues[Math.max(3, filters.size)].slice(0).reverse()
-		option.series = [];
-		filters.forEach(filterId => {
-			let series = { 
-				type: 'bar', 
-				label: { 
-					show: true, 
-					position: 'top',
-					formatter: (o) => {
-						let rows = [i18n.t('data.value') +': ' + format(this.indicatorType, o.data.value)];
-						if (o.data.moeLow || o.data.moeHigh) {
-							if (o.data.moeLow === o.data.moeHigh) {
-								rows.push(i18n.t('data.moe') + ': ' + format(this.indicatorType, o.data.moeLow))
-							} else {
-								rows.push(i18n.t('data.moe') + ' (' + i18n.t('data.high') + '): ' + format(this.indicatorType, o.data.moeHigh));
-								rows.push(i18n.t('data.moe') + ' (' + i18n.t('data.low') + '): ' + format(this.indicatorType, o.data.moeLow));
-							}
-						}
-						return rows.join('\n');
-					}
-				}
-			};
-			if (filterId !== 0) {
-				series.name = this.data.find(d => d.baseFilter.id === filterId).baseFilter['name_' + this.locale]
+			if (this.data[0].baseFilter) {
+				option.legend = { data: this.data.map(d => d.baseFilter['name_' + this.locale])};
 			}
-			series.data = this.data
-				.filter(d => (d.baseFilter?.id || 0) === filterId)
-				.map(dataPoint => { 
-					return { value: dataPoint.value, moeLow: dataPoint.moeLow, moeHigh: dataPoint.moeHigh }; 
-				})
-			option.series.push(series)
-		});
-		this.chart.setOption(option);
+			option.yAxis = { 
+				type: 'value', 
+				splitLine: { show: false }
+			};
+			option.xAxis = { 
+				type: 'category', 
+				data: Array.from(new Set(this.data.map(d => d.raceFilter['name_' + this.locale]))),
+				axisTick: { show: false }
+			};
+			let filters = new Set(this.data.map(d => d.baseFilter?.id || 0))
+			option.color = colorbrewer.Blues[Math.max(3, filters.size)].slice(0).reverse()
+			option.series = [];
+			filters.forEach(filterId => {
+				let series = { 
+					type: 'bar', 
+					cursor: 'default',
+					label: { 
+						show: true, 
+						position: 'top',
+						formatter: (o) => {
+							let rows = [i18n.t('data.value') +': ' + format(this.indicatorType, o.data.value)];
+							if (o.data.moeLow || o.data.moeHigh) {
+								if (o.data.moeLow === o.data.moeHigh) {
+									rows.push(i18n.t('data.moe') + ': ' + format(this.indicatorType, o.data.moeLow))
+								} else {
+									rows.push(i18n.t('data.moe') + ' (' + i18n.t('data.high') + '): ' + format(this.indicatorType, o.data.moeHigh));
+									rows.push(i18n.t('data.moe') + ' (' + i18n.t('data.low') + '): ' + format(this.indicatorType, o.data.moeLow));
+								}
+							}
+							return rows.join('\n');
+						}
+					}
+				};
+				if (filterId !== 0) {
+					series.name = this.data.find(d => d.baseFilter.id === filterId).baseFilter['name_' + this.locale]
+				}
+				series.data = this.data
+					.filter(d => (d.baseFilter?.id || 0) === filterId)
+					.map(dataPoint => { 
+						return { value: dataPoint.value, moeLow: dataPoint.moeLow, moeHigh: dataPoint.moeHigh }; 
+					})
+				option.series.push(series)
+			});
+			this.chart.setOption(option);
 		}
 	},
 }
