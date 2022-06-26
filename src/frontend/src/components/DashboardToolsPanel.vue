c2e939d5ea000bbef6b88295663afaeff9850b85
Brian <herronrb@gvsu.edu>
2022-06-26 12:34:04 -0400

    dashboard compare progress

--- src/frontend/src/components/DashboardToolsPanel.vue
+++ src/frontend/src/components/DashboardToolsPanel.vue
@@ -5,6 +5,10 @@
 				label="Compare by"
 				dense
 				flat
+				return-object
+				:items="compareByItems"
+				:item-text="'name_' + locale"
+				v-model="compareBy"
 			>
 			</v-select>
 		</v-col>
@@ -13,7 +17,12 @@
 				label="Compare with"
 				dense
 				flat
+				return-object
+				:items="compareWithItems"
+				:item-text="'name_' + locale"
+				v-model="compareWith"
 				multiple
+				
 			>
 			</v-select>
 		</v-col>
@@ -32,14 +41,45 @@
 </template>
 
 <script>
-	export default {
-		name: 'DashboardToolsPanel'
-		//TODO: set the options for the 'by' select
-		// then dynamically set the options for the 'with' select
-		// then handle compare click event
-		// first validate that a by and with are selected
-		// then fetch data - will need API updates for by and multiple withs
-	}
+
+import { mapState } from 'vuex'
+
+export default {
+	name: 'DashboardToolsPanel',
+	//TODO: set the options for the 'by' select
+	// then dynamically set the options for the 'with' select
+	// then handle compare click event
+	// first validate that a by and with are selected
+	// then fetch data - will need API updates for by and multiple withs
+	computed: {
+		...mapState(['filters', 'locale']),
+		compareByItems() {
+			let items = [];
+			items.push(this.filters?.locationFilter.type);
+			this.filters?.indicatorFilters.forEach(filter => {
+				items.push(filter.type)
+			});
+			
+			return items;
+		},
+		compareWithItems() {
+			let items = [];
+			if (this.compareBy?.name_en === 'Location') {
+				items = this.filters?.locationFilter.options;
+			} else {
+				items = this.filters?.indicatorFilters.find(filter => filter.type.name_en === this.compareBy?.name_en)?.options;
+			}
+			
+			return items;
+		}
+	},
+	data() {
+		return {
+			compareBy: null,
+			compareWith: null
+		}
+	},
+}
 </script>
 
 <style lang="scss" scoped>
