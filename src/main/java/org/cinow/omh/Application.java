f21a8d929a5b530ebc531a06d6985dd733a546ab
brian <briancherron@pm.me>
2022-07-31 13:50:57 -0400

    app and config javadoc

--- src/main/java/org/cinow/omh/Application.java
+++ src/main/java/org/cinow/omh/Application.java
@@ -5,9 +5,19 @@ import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The Spring Boot app.
+ * 
+ * @author brian
+ */
 @SpringBootApplication
 public class Application {
 
+	/**
+	 * Main method.
+	 * @param args the command line arguments
+	 * @throws IOException if there is an error
+	 */
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(Application.class, args);
 	}
--- src/main/java/org/cinow/omh/WebConfig.java
+++ src/main/java/org/cinow/omh/WebConfig.java
@@ -5,21 +5,32 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * The web configuration.
+ * 
+ * @author brian
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/")
-                .setViewName("forward:/index.html");
-        registry.addViewController("/{x:[\\w\\-]+}")
-                .setViewName("forward:/index.html");
-        registry.addViewController("/{x:^(?!api$).*$}/*/{y:[\\w\\-]+}")
-                .setViewName("forward:/index.html");
-	}
 
-	@Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
-    }
+        /**
+         * Adds the view controllers. All views will be forwarded to index.html as Vue handles all routing.
+         */
+        @Override
+        public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/")
+                                .setViewName("forward:/index.html");
+                registry.addViewController("/{x:[\\w\\-]+}")
+                                .setViewName("forward:/index.html");
+                registry.addViewController("/{x:^(?!api$).*$}/*/{y:[\\w\\-]+}")
+                                .setViewName("forward:/index.html");
+        }
+
+        /**
+         * Adds the resource handlers for static content.
+         */
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        }
 }
