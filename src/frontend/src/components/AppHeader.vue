3ae8b35266feee0de4f2f55de8e26c247fb77233
brian <briancherron@pm.me>
2022-08-08 20:56:50 -0400

    added cinow logo

--- src/frontend/src/components/AppHeader.vue
+++ src/frontend/src/components/AppHeader.vue
@@ -2,14 +2,14 @@
   <v-app-bar app color="primary" dark>
     <v-toolbar-title>
       [BDD logo]
-      {{ $t('app.name') }} <sup class="text-caption">ALPHA</sup>
+      {{ $t('app.name') }} <sup class="text-caption">BETA</sup>
     </v-toolbar-title>
     <v-spacer></v-spacer>
     <v-btn-toggle :value="locale" mandatory group dark>
       <v-btn text tile plain value="en" @click="setLocale('en')">English</v-btn>
       <div style="margin-top: 4px;" title="Coming soon"><v-btn text tile plain value="es" @click="setLocale('es')" disabled>Español</v-btn></div>
     </v-btn-toggle>
-    <span>[CI:Now logo]</span>
+    <span><img src="/img/cinow_logo.png" style="height: 30px;" /></span>
   </v-app-bar>
 </template>
 
