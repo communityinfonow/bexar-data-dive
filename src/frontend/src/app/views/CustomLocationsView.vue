75759b77acde2b6baef294ec290f5a3a431c5e1b
brian <briancherron@pm.me>
2023-04-26 10:00:42 -0400

    more updates for filters, etc.

--- src/frontend/src/app/views/CustomLocationsView.vue
+++ src/frontend/src/app/views/CustomLocationsView.vue
@@ -11,15 +11,6 @@
       <v-col cols="auto" class="pa-4 shrink">
         <div class="d-flex justify-space-between">
           <h1 class="text-h3 mb-2">{{ $t('tools.custom_locations.name') }}</h1>
-          <v-btn
-            icon
-            color="accent"
-            class="mt-4"
-            @click="viewCustomLocations"
-            :title="$t('tools.custom_locations.open')"
-          >
-            <v-icon>mdi-folder-open</v-icon>
-          </v-btn>
         </div>
         <p>{{ $t('tools.custom_locations.long_description') }}</p>
         <p>{{ $t('tools.custom_locations.get_started') }}</p>
@@ -59,61 +50,123 @@
           </l-map>
         </v-col>
         <v-col cols="4">
-          <section ref="selectionPanel" class="d-flex flex-column justify-start">
-            <v-select
-              v-if="layers.length"
-              :items="layers"
-              :label="$t('tools.community.community_types')"
-              :item-text="item => item['name_' + locale]"
-              :item-value="item => item.id"
-              :menu-props="{ maxHeight: '400' }"
-              :return-object="true"
-              v-model="selectedLayer"
-              @change="selectLocationType"
-            ></v-select>
-            <v-list v-if="selectionGeojson" style="overflow-y: auto;">
-              <v-list-item-group multiple v-model="customLocationGeojson.features">
-                <v-list-item v-for="location in selectionGeojson.features" :key="location.properties.id" :value="location">
-                  <template v-slot:default="{active}">
-                    <v-list-item-action>
-                      <v-checkbox
-                        color="accent"
-                        :input-value="active"
-                      ></v-checkbox>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                      <v-list-item-title>{{ location.properties.name }}</v-list-item-title>
-                    </v-list-item-content>
-                  </template>
-                </v-list-item>
-              </v-list-item-group>
-            </v-list>
-            <v-spacer></v-spacer>
+          <v-form ref="selectionForm" lazy-validation v-model="selectionForm" class="d-flex flex-column justify-start">
+            <v-btn
+              color="accent"
+              @click="viewCustomLocations"
+            >
+              <v-icon left>mdi-folder-open</v-icon>
+              {{ $t('tools.custom_locations.open')}}
+            </v-btn>
+            <span class="text-button text-center font-italic mt-2">- {{ $t('tools.custom_locations.or') }} -</span>
+            <h2 class="text-button text-center">{{ $t('tools.custom_locations.create') }}</h2>
+            <h2 class="text-subtitle-1">{{ $t('tools.custom_locations.step_1') }}</h2>
             <v-text-field
               v-if="customLocationGeojson"
               :label="$t('tools.custom_locations.location_name')"
               v-model="customLocation.name"
+              :hint="customLocations.find(l => customLocation.name && l.name === customLocation.name) ? $t('tools.custom_locations.overwrite') : null"
+              persistent-hint
+              :rules="[rules.name_required, rules.custom_name]"
             ></v-text-field>
-            <div class="d-flex flex-column">
+            <h2 class="text-subtitle-1">{{ $t('tools.custom_locations.step_2') }}</h2>
+            <v-select
+              v-if="layers.length"
+              :items="layers"
+              :label="$t('tools.common.download.headers.location_type')"
+              :item-text="item => item['name_' + locale]"
+              :item-value="item => item.id"
+              :return-object="true"
+              v-model="selectedLayer"
+              @change="selectLocationType"
+            ></v-select>
+            <v-row class="selection-lists mb-2" style="overflow-y: auto;">
+              <v-col :md="6" :sm="12" v-if="$vuetify.breakpoint.smAndDown">
+                <span class="text-subtitle-2">{{ $t('tools.custom_locations.selected_locations') }}</span>
+                <p v-if="!customLocationGeojson.features.length" class="mt-2 mb-0 font-italic">{{ $t('tools.custom_locations.no_locations_selected') }}</p>
+                <v-list v-if="selectionGeojson" style="overflow-y: auto;">
+                  <v-list-item-group multiple v-model="customLocationGeojson.features">
+                    <v-list-item v-for="location in customLocationGeojson.features" :key="location.properties.id" :value="location">
+                      <template v-slot:default="{active}">
+                        <v-list-item-action>
+                          <v-checkbox
+                            color="accent"
+                            :input-value="active"
+                          ></v-checkbox>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                          <v-list-item-title>{{ location.properties.name }}</v-list-item-title>
+                        </v-list-item-content>
+                      </template>
+                    </v-list-item>
+                  </v-list-item-group>
+                </v-list>
+              </v-col>
+              <v-col :md="6" :sm="12">
+                <span class="text-subtitle-2">{{ $t('tools.custom_locations.available_locations') }}</span>
+                <!-- TODO: message turns red after checking a box when it shouldn't until form submit -->
+                <p class="mt-2 mb-0 font-italic" :class="!selectionForm && customLocationGeojson.features.length < 2 ? 'error--text' : ''">{{ $t('tools.custom_locations.two_locations') }}</p>
+                <v-list v-if="selectionGeojson" style="overflow-y: auto;">
+                  <v-list-item-group multiple v-model="customLocationGeojson.features">
+                    <template v-for="location in selectionGeojson.features">
+                      <v-list-item v-show="!customLocationGeojson.features.some(f => f.properties.id === location.properties.id)" :key="location.properties.id" :value="location">
+                        <template v-slot:default="{active}">
+                          <v-list-item-action>
+                            <v-checkbox
+                              color="accent"
+                              :input-value="active"
+                              :rules="[rules.two_locations]"
+                            ></v-checkbox>
+                          </v-list-item-action>
+                          <v-list-item-content>
+                            <v-list-item-title>{{ location.properties.name }}</v-list-item-title>
+                          </v-list-item-content>
+                        </template>
+                      </v-list-item>
+                    </template>
+                  </v-list-item-group>
+                </v-list>
+              </v-col>
+              <v-col :md="6" :sm="12" v-if="$vuetify.breakpoint.mdAndUp">
+                <span class="text-subtitle-2">{{ $t('tools.custom_locations.selected_locations') }}</span>
+                <p v-if="!customLocationGeojson.features.length" class="mt-2 mb-0 font-italic">{{ $t('tools.custom_locations.no_locations_selected') }}</p>
+                <v-list v-if="selectionGeojson" style="overflow-y: auto;">
+                  <v-list-item-group multiple v-model="customLocationGeojson.features">
+                    <v-list-item v-for="location in customLocationGeojson.features" :key="location.properties.id" :value="location">
+                      <template v-slot:default="{active}">
+                        <v-list-item-action>
+                          <v-checkbox
+                            color="accent"
+                            :input-value="active"
+                          ></v-checkbox>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                          <v-list-item-title>{{ location.properties.name }}</v-list-item-title>
+                        </v-list-item-content>
+                      </template>
+                    </v-list-item>
+                  </v-list-item-group>
+                </v-list>
+              </v-col>
+            </v-row>
+            <v-spacer></v-spacer>
+            <div class="d-flex">
               <v-btn
-                :disabled="!customLocation.name || customLocationGeojson.features.length < 2"
                 color="primary"
-                class="mt-4"
+                class="mt-4 mr-2 flex-grow-1"
                 @click="saveCustomLocation"
               >
                 {{ $t('tools.custom_locations.save') }}
               </v-btn>
               <v-btn
-                text
-                :disabled="!customLocation.name || customLocationGeojson.features.length < 2"
-                color="accent"
-                class="mt-4"
+                color="primary"
+                class="mt-4 ml-2 flex-grow-1"
                 @click="exportCustomLocation"
               >
                 {{ $t('tools.custom_locations.export') }}
               </v-btn>
             </div>
-          </section>
+          </v-form>
         </v-col>
       </v-col>
     </v-row>
@@ -122,27 +175,51 @@
         <v-card-title class="headline">{{ $t('tools.custom_locations.open') }}</v-card-title>
         <v-card-text>
           <v-card-subtitle>{{ $t('tools.custom_locations.open_description') }}</v-card-subtitle>
-          <v-alert v-if="!customLocations.length" type="info">{{ $t('tools.custom_locations.no_locations') }}</v-alert>
+          <v-alert v-if="!customLocations.length" type="info">{{ $t('tools.custom_locations.no_locations_saved') }}</v-alert>
           <v-list>
             <v-list-item-group v-model="customLocationSelected" color="primary">
               <v-list-item v-for="location in customLocations" :key="location.key" :value="location">
                 <v-list-item-content>
-                    <v-list-item-title>{{ location.name }}</v-list-item-title>
-                  </v-list-item-content>
+                  <v-list-item-title>{{ location.name }}</v-list-item-title>
+                </v-list-item-content>
+                <v-list-item-action class="flex-row">
+                  <v-btn text inline color="primary" @click="viewCommunity(location)">
+                    {{ $t('tools.custom_locations.view_community') }}
+                  </v-btn>
+                  <v-btn text inline @click="removeCustomLocation(location.key)">
+                    {{ $t('tools.custom_locations.delete') }}
+                  </v-btn>
+                </v-list-item-action>
               </v-list-item>
             </v-list-item-group>
           </v-list>
-          <span>TODO: maybe go for a file upload instead of copy/paste code?</span>
-          <v-text-field v-model="customLocationCode" :label="$t('tools.custom_locations.code')" prepend-inner-icon="mdi-code-braces" outlined></v-text-field>
+          <v-file-input accept=".json" :label="$t('tools.custom_locations.load')" prepend-icon="mdi-upload" outlined v-model="customLocationFile"></v-file-input>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="primary" text :disabled="!customLocationSelected && !customLocationCode" @click="openCustomLocation">{{ $t('tools.custom_locations.open') }}</v-btn>
+          <v-btn color="primary" text :disabled="!customLocationSelected && !customLocationFile" @click="openCustomLocation">{{ $t('tools.custom_locations.open') }}</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
-    <v-snackbar v-model="message" timeout="2000" color="success" class="mb-8">
+    <v-snackbar v-model="message" light multiLine timeout="-1" class="mb-8">
 			{{ messageText }}
+       <template v-slot:action="{ attrs }">
+        <v-btn
+          text
+          color="primary"
+          v-bind="attrs"
+          @click="viewCommunity(customLocation)"
+        >
+          i18n.t('tools.custom_locations.view_community')
+        </v-btn>
+        <v-btn
+          text
+          v-bind="attrs"
+          @click="message = false"
+        >
+          i18n.t('tools.custom_locations.dismiss')
+        </v-btn>
+      </template>
 		</v-snackbar>
   </v-container>
 </template>
@@ -169,8 +246,9 @@ export default {
       zoom: 10,
 			center: latLng(29.43445, -98.473562383),
       selectedLayer: null,
+      selectionForm: null,
       selectionGeojson: null,
-      customLocationCode: null,
+      customLocationFile: null,
       customLocationSelected: null,
       customLocation: { key: null, name: null, locationTypeId: null, locationIds: []},
       customLocationGeojson: featureCollection([]),
@@ -230,6 +308,23 @@ export default {
       return {
         onEachFeature: this.onEachCustomFeature
       }
+    },
+    rules() {
+      return {
+        name_required: v => !!v || i18n.t('tools.custom_locations.name_required'),
+        custom_name: this.locationMenu.categories
+          .filter(c => c.id !== "7")
+          .map(c => c.items)
+          .reduce((acc, curr) => acc.concat(curr), [])
+          .map(l => l.name_en.toLowerCase())
+          .concat(this.locationMenu.categories
+            .filter(c => c.id !== "7")
+            .map(c => c.items)
+            .reduce((acc, curr) => acc.concat(curr), [])
+            .map(l => l.name_es.toLowerCase()))
+          .indexOf(this.customLocation.name?.toLowerCase()) === -1 || i18n.t('tools.custom_locations.custom_name'),
+        two_locations: this.customLocationGeojson.features.length > 1 || i18n.t('tools.custom_locations.two_locations')
+      }
     }
   },
   watch: {
@@ -260,7 +355,7 @@ export default {
     
   },
   methods: {
-    ...mapActions(['setCommunity', 'setToolRoute', 'addCustomLocation']),
+    ...mapActions(['setCommunity', 'setToolRoute', 'addCustomLocation', 'deleteCustomLocation']),
     selectLocation(feature) {
       console.log(feature)
       if (this.customLocationGeojson.features.find(f => f.id === feature.id)) {
@@ -275,7 +370,7 @@ export default {
 		},
     resizeHandler() {
 			this.$refs.selectionMap?.mapObject?.invalidateSize();
-      this.$refs.selectionPanel.style.height = this.$refs.selectionMap.$el.clientHeight + 'px';
+      this.$refs.selectionForm.$el.style.height = this.$refs.selectionMap.$el.clientHeight + 'px';
 		},
     selectLocationType() {
       this.customLocation.locationTypeId = this.selectedLayer.id;
@@ -322,37 +417,79 @@ export default {
       this.openDialog = true;
     },
     openCustomLocation() {
-      this.customLocation = this.customLocationSelected ? Object.assign({}, this.customLocationSelected) : JSON.parse(this.customLocationCode);
+      if (this.customLocationSelected) {
+        this.customLocation = Object.assign({}, this.customLocationSelected);
+        this.loadCustomLocation();
+      } else {
+        let reader = new FileReader();
+        reader.addEventListener('load', () => {
+          this.customLocation = JSON.parse(reader.result);
+          this.loadCustomLocation();
+        });
+        reader.readAsText(this.customLocationFile);
+      }
+    },
+    loadCustomLocation() {
       //TODO: validate customLocationCode to ensure it is a valid custom location json object
       this.customLocationSelected = null;
-      this.customLocationCode = null;
+      this.customLocationFile = null;
       this.selectedLayer = this.layers.find(l => l.id === this.customLocation.locationTypeId);
       this.drawSelectionMap().then(() => {
         this.customLocationGeojson = featureCollection(this.selectionGeojson.features.filter(f => this.customLocation.locationIds.includes(f.id)));
         this.openDialog = false;
+        this.$router.replace({
+        query: {
+          ...this.$router.currentRoute.query,
+          locationTypeId: this.customLocation.locationTypeId,
+          locationIds: this.customLocation.locationIds,
+          locationName: this.customLocation.name,
+
+        }
+      });
       });
     },
     saveCustomLocation() {
-      this.customLocation.key = crypto.randomUUID();
-      this.customLocation.locationIds = this.customLocationGeojson.features.map(f => f.id)
-      this.customLocation.geojson = this.customLocationGeojson;
-      this.addCustomLocation(this.customLocation);
-      this.messageText = i18n.t('tools.custom_locations.saved');
-      this.message = true;
-      //TODO: need feedback that the custom location was saved
+      if (this.$refs.selectionForm.validate()) {
+        this.customLocation.key = crypto.randomUUID();
+        this.customLocation.locationIds = this.customLocationGeojson.features.map(f => f.id)
+        this.customLocation.geojson = this.customLocationGeojson;
+        this.addCustomLocation(this.customLocation);
+        this.messageText = i18n.t('tools.custom_locations.saved');
+        this.message = true;
+      }
     },
     exportCustomLocation() {
-      let downloadLink = document.createElement('a');
-      downloadLink.download = 'bexar_data_dive__' + this.customLocation.name + '.json';
-      downloadLink.href = 'data:text/csv;charset=utf-8,' + encodeURIComponent(JSON.stringify(this.customLocation));
-      downloadLink.click();
+      if (this.$refs.selectionForm.validate()) {
+        let downloadLink = document.createElement('a');
+        downloadLink.download = 'Dive Custom Location - ' + this.customLocation.name + '.json';
+        downloadLink.href = 'data:text/csv;charset=utf-8,' + encodeURIComponent(JSON.stringify(this.customLocation));
+        downloadLink.click();
+      }
+    },
+    removeCustomLocation(key) {
+      this.deleteCustomLocation(key);
+    },
+    viewCommunity(customLocation) {
+      this.$router.push({
+        name: 'community',
+        query: {
+          lang: this.locale,
+          locationType: customLocation.locationTypeId,
+          locations: customLocation.locationIds,
+          name: customLocation.name,
+        }
+      });
     }
-  },
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .open-dialog {
   z-index: 9999;
+}
+.selection-lists .v-list-item__title {
+  text-overflow: initial;
+  white-space: initial;
 }
 </style>
