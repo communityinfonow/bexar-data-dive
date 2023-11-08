87798a9c610a2693df9ec5502cecc6bf321d69cf
brian <briancherron@pm.me>
2023-11-08 16:48:44 -0500

    faq layour adjustment, font size increases

--- src/frontend/src/app/views/FaqsView.vue
+++ src/frontend/src/app/views/FaqsView.vue
@@ -1,16 +1,16 @@
 <template>
   <v-container v-if="faqs" fluid class="pa-0">
-    <v-row class="no-gutters">
-      <v-col cols="7" class="pa-4 col col-auto">
-        <section v-for="item in faqs" :key="item.id" :class="item.id === 1 ? 'mt-4' : ''" :id="'question_' + item.id">
-          <v-divider v-if="item.id !== 1" class="my-12"></v-divider>
-          <h2 class="mb-4 text-dive-h5">{{ item['question_' + locale]}}</h2>
-          <section class="mb-8">
-            <section v-html="item['answer_' + locale]"></section>
-          </section>
-        </section>
-      </v-col>
-    </v-row>
+    <div  v-for="item in faqs" :key="item.id" :class="'mx-8 mb-8 ' + (item.id === 1 ? 'mt-4' : '')" :id="'question_' + item.id">
+      <v-divider v-if="item.id !== 1" class="my-12"></v-divider>
+      <v-row class="no-gutters">
+        <v-col cols="12" lg="4" class="pa-4 col col-auto">
+          <h2 class="mb-4 text-dive-h5 blue--text">{{ item['question_' + locale]}}</h2>
+        </v-col>
+        <v-col cols="12" lg="8" class="pa-4 col col-auto" style="font-size: 1.25rem">
+          <section v-html="item['answer_' + locale]"></section>
+        </v-col>
+      </v-row>
+    </div>
   </v-container>
 </template>
 
