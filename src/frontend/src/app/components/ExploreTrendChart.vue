9a6f428c333a2ab8814b24f6eeb48cc8e93bdf3d
brian <briancherron@pm.me>
2023-12-18 17:11:03 -0500

    initial trend compare progress

--- src/frontend/src/app/components/ExploreTrendChart.vue
+++ src/frontend/src/app/components/ExploreTrendChart.vue
@@ -3,6 +3,7 @@
 		<v-row class="no-gutters flex-wrap flex-column fill-height">
 			<explore-tools-panel 
 				v-if="filters"
+				:showCompareOptions="true"
 				:labelsOrLinesOption="trendLabelsOrLines"
 				:setLabelsOrLinesOption="setTrendLabelsOrLines"
 				dataVisualElementId="trend_chart_container"
@@ -27,7 +28,7 @@ import { mapActions, mapState, mapGetters } from 'vuex'
 import i18n from '@/i18n'
 import * as echarts from 'echarts/core';
 import { SVGRenderer } from 'echarts/renderers';
-import { AriaComponent, LegendComponent, GridComponent } from 'echarts/components';
+import { AriaComponent, LegendComponent, GridComponent, TooltipComponent } from 'echarts/components';
 import { LineChart, BarChart, CustomChart } from 'echarts/charts';
 import ExploreToolsPanel from '@/app/components/ExploreToolsPanel'
 import { format } from '@/services/formatter'
@@ -44,11 +45,12 @@ export default {
 	},
 	data() {
 		return {
-			chart: null
+			chart: null,
+			seriesColors: ['#3B5A98', '#F05A28', '#F6921E', '#B8237E', '#3AA38F', '#84ACCE', '#E77478', '#663F46', '#C5DCA0'],
 		}
 	},
 	computed: {
-		...mapState(['locale', 'exploreData', 'trendLabelsOrLines', 'exploreTab']),
+		...mapState(['locale', 'exploreData', 'compareSelections', 'trendLabelsOrLines', 'exploreTab', 'indicator']),
 		...mapGetters(['filters']),
 	},
 	watch: {
@@ -74,7 +76,7 @@ export default {
 	},
 	mounted () {
 		setTimeout(() => { 
-			echarts.use([SVGRenderer, AriaComponent, LegendComponent, GridComponent, LineChart, BarChart, CustomChart]);
+			echarts.use([SVGRenderer, AriaComponent, LegendComponent, GridComponent, LineChart, BarChart, CustomChart, TooltipComponent]);
 			this.chart = echarts.init(document.getElementById('trend_chart_container'), null, { renderer: 'svg'});
 			window.addEventListener('resize', () => {
 				if (this.exploreTab === 'trend') {
@@ -96,6 +98,8 @@ export default {
 			this.chart = echarts.init(document.getElementById('trend_chart_container'), null, { renderer: 'svg'});
 			this.chart.on('mouseover', (params) => {
 				if (params.componentType === 'series') {
+					console.log(params)
+					let comparedIndicatorFilters = JSON.parse(JSON.stringify(params.data.indicatorFilters));
 					this.setDockedTooltip({
 						value: params.data.value,
 						noData: params.data.noData,
@@ -104,7 +108,7 @@ export default {
 						moeHigh: params.data.moeHigh,
 						location: this.exploreData.filters.locationFilter.options[0]['name_' + this.locale],
 						year: params.name,
-						indicatorFilters: this.exploreData.filters.indicatorFilters
+						indicatorFilters: comparedIndicatorFilters
 					});
 				}
 			});
@@ -133,139 +137,177 @@ export default {
 				axisTick: { show: false },
 				axisLabel: Object.assign({}, textStyle)
 			};
-			option.color = '#3b5a98';
-			let yearData = this.exploreData.locationData.find(ld => 
+			let seriesNames = [];
+			if (this.exploreData.compareData) {
+				if (this.compareSelections.type.id === 'i') {
+					seriesNames.push(this.indicator['name_' + this.locale]);
+				} else if (this.compareSelections.type.id === 'l') {
+					seriesNames.push(this.exploreData.filters.locationFilter.options[0]['name_' + this.locale]);
+				} else if (this.compareSelections.type.id === 'y') {
+					seriesNames.push(this.exploreData.filters.yearFilter.options[0]['name_' + this.locale]);
+				} else {
+					seriesNames.push(this.exploreData.filters.indicatorFilters.find(f => f.type.id === this.compareSelections.type.id).options[0]['name_' + this.locale])
+				}
+				seriesNames.push(...this.compareSelections.options.filter(o => !!o).map(o => o['name_' + this.locale]))
+			} else {
+				seriesNames.push('')
+			}
+			//option.color = '#3b5a98';
+			let seriesData = [this.exploreData.locationData.find(ld => 
 						ld.location.id === this.exploreData.filters.locationFilter.options[0].id && 
-						ld.location.typeId === this.exploreData.filters.locationTypeFilter.options[0].id)?.yearData;
-			option.series = [{
-				// first series for valid values
-				data: trendYears
-					.map(ty => {
-						let yd = yearData[ty]; 
-						return { 
-							value: yd?.value, 
-							noData: yd?.value === null,
-							suppressed: yd?.suppressed,
-							moeLow: yd?.moeLow, 
-							moeHigh: yd?.moeHigh
-						}; 
-					}),
-				type: 'line',
-				cursor: 'default',
-				emphasis: {
-					disabled: true
-				},
-				symbol: 'circle',
-				symbolSize: 12,
-				label: {
-					show: this.trendLabelsOrLines === 'labels',
-					position: 'top',
-					formatter: (o) => {
-						if (o.data.suppressed) {
-							return '';
-						} else if (o.data.noData) {
-							return '';
-						} else if (this.trendLabelsOrLines === 'labels') {
-							let rows = ['{a|' + i18n.t('data.value') +': ' + format(this.exploreData.indicator.typeId, o.data.value) + '}'];
-							if (o.data.moeLow || o.data.moeHigh) {
-								rows.push('{b|' + i18n.t('data.moe_range') 
-									+ ': ' 
-									+ format(this.exploreData.indicator.typeId, o.data.moeLow)
-									+ " - "
-									+ format(this.exploreData.indicator.typeId, o.data.moeHigh) + '}');
-							}
-							return rows.join('\n');
-						} else {
-							return '';
-						}
+						ld.location.typeId === this.exploreData.filters.locationTypeFilter.options[0].id)?.yearData
+			];
+			seriesData[0].indicatorFiltrers = this.exploreData.filters.indicatorFilters
+			if (this.exploreData.compareData) {
+				seriesData.push(...this.exploreData.compareData.map((cd, index) => {
+					let compareIndicatorFilters = JSON.parse(JSON.stringify(this.exploreData.filters.indicatorFilters));
+					if (!isNaN(this.compareSelections.type.id)) {
+						compareIndicatorFilters.find(f => f.type.id === this.compareSelections.type.id).options[0] = this.compareSelections.options[index];
+					}
+					return {
+						...cd.yearData,
+						indicatorFilters: compareIndicatorFilters	
+					}
+				}))
+			}
+			console.log(seriesData)
+			option.series = []
+			let allValues = []
+			seriesData.forEach((indicatorData, index) => {
+				allValues = allValues.concat(Object.values(indicatorData).map(yd => yd.value || 0));
+				if (this.trendLabelsOrLines === 'lines') {
+					allValues = allValues.concat(...Object.values(indicatorData).map(yd => yd.moeHigh || 0));
+					allValues = allValues.concat(...Object.values(indicatorData).map(yd => yd.moeLow || 0));
+				}
+				option.series.push({
+					// first series for valid values
+					name: seriesNames[index],
+					data: trendYears
+						.map(ty => {
+							let yd = indicatorData[ty]; 
+							return { 
+								value: yd?.value, 
+								noData: yd?.value === null,
+								suppressed: yd?.suppressed,
+								moeLow: yd?.moeLow, 
+								moeHigh: yd?.moeHigh,
+								indicatorFilters: indicatorData.indicatorFilters
+							}; 
+						}),
+					type: 'line',
+					cursor: 'default',
+					color: this.seriesColors[index],
+					emphasis: {
+						disabled: true
 					},
-					rich: { 
-						a: {
-							align: 'center',
-							fontSize: '16px',
-							lineHeight: '20',
-							color: '#333333'
+					symbol: 'circle',
+					symbolSize: 12,
+					label: index > 0 ? { show: false } : {
+						show: this.trendLabelsOrLines === 'labels',
+						position: 'top',
+						formatter: (o) => {
+							if (o.data.suppressed) {
+								return '';
+							} else if (o.data.noData) {
+								return '';
+							} else if (this.trendLabelsOrLines === 'labels') {
+								let rows = ['{a|' + i18n.t('data.value') +': ' + format(this.exploreData.indicator.typeId, o.data.value) + '}'];
+								if (o.data.moeLow || o.data.moeHigh) {
+									rows.push('{b|' + i18n.t('data.moe_range') 
+										+ ': ' 
+										+ format(this.exploreData.indicator.typeId, o.data.moeLow)
+										+ " - "
+										+ format(this.exploreData.indicator.typeId, o.data.moeHigh) + '}');
+								}
+								return rows.join('\n');
+							} else {
+								return '';
+							}
 						},
-						b: {
-							align: 'center',
-							fontSize: '14px',
-							lineHeight: '16',
-							color: '#666666'
+						rich: { 
+							a: {
+								align: 'center',
+								fontSize: '16px',
+								lineHeight: '20',
+								color: '#333333'
+							},
+							b: {
+								align: 'center',
+								fontSize: '14px',
+								lineHeight: '16',
+								color: '#666666'
+							}
 						}
 					}
-				}
-			},
-			{
-				// second series for nulls/no data
-				data: trendYears
-					.map(ty => {
-						let yd = yearData[ty];
-						if (yd?.value !== null && !yd?.suppressed) {
-							return null;
-						}
-						return { 
-							value: 0, 
-							noData: yd?.value === null,
-							suppressed: yd?.suppressed,
-							moeLow: null, 
-							moeHigh: null
-						}; 
-					}),
-				type: 'bar',
-				cursor: 'default',
-				label: {
-					show: true,
-					position: 'top',
-					formatter: (o) => {
-						return o.data.suppressed ? i18n.t('data.suppressed') : (!o.data.value ? i18n.t('data.no_data') : o.data.value)
-					}
-				}
-			}];
-			if (this.trendLabelsOrLines === 'lines') {
+				})
 				option.series.push({
+					// second series for nulls/no data
 					data: trendYears
 						.map(ty => {
-							let yd = yearData[ty]; 
+							let yd = indicatorData[ty];
+							if (yd?.value !== null && !yd?.suppressed) {
+								return null;
+							}
 							return { 
-								value: yd?.moeLow, 
+								value: 0, 
 								noData: yd?.value === null,
 								suppressed: yd?.suppressed,
-								moeLow: yd?.moeLow, 
-								moeHigh: yd?.moeHigh
-							};
+								moeLow: null, 
+								moeHigh: null
+							}; 
 						}),
-					type: 'line',
-					stack: 'moe',
-					lineStyle: { opacity: 0 },
-					itemStyle: { opacity: 0 },
-					areaStyle: { opacity: 0 }
+					type: 'bar',
+					cursor: 'default',
+					label: {
+						show: true,
+						position: 'top',
+						formatter: (o) => {
+							return o.data.suppressed ? i18n.t('data.suppressed') : (!o.data.value ? i18n.t('data.no_data') : o.data.value)
+						}
+					}
+				})
+				if (this.trendLabelsOrLines === 'lines' && index === 0) {
+					option.series.push({
+						data: trendYears
+							.map(ty => {
+								let yd = indicatorData[ty]; 
+								return { 
+									value: yd?.moeLow, 
+									noData: yd?.value === null,
+									suppressed: yd?.suppressed,
+									moeLow: yd?.moeLow, 
+									moeHigh: yd?.moeHigh
+								};
+							}),
+						type: 'line',
+						stack: 'moe',
+						lineStyle: { opacity: 0 },
+						itemStyle: { opacity: 0 },
+						areaStyle: { opacity: 0 }
 
-				});
-				option.series.push({
-					data: trendYears
-						.map(ty => {
-							let yd = yearData[ty]; 
-							return { 
-								value: yd?.moeHigh - yd?.moeLow, 
-								noData: yd?.value === null,
-								suppressed: yd?.suppressed,
-								moeLow: yd?.moeLow, 
-								moeHigh: yd?.moeHigh
-							};
-						}),
-					type: 'line',
-					stack: 'moe',
-					lineStyle: { opacity: 0 },
-					itemStyle: { opacity: 0 },
-					areaStyle: { opacity: 0.25 },
-					color: '#b8237e'
-				});
-			}
-			let allValues = option.series[0].data.map(d => d.value);
-			if (this.trendLabelsOrLines === 'lines') {
-				allValues = allValues.concat(...option.series[0].data.map(d => d.moeHigh || 0));
-				allValues = allValues.concat(...option.series[0].data.map(d => d.moeLow || 0));
-			}
+					});
+					option.series.push({
+						data: trendYears
+							.map(ty => {
+								let yd = indicatorData[ty]; 
+								return { 
+									value: yd?.moeHigh - yd?.moeLow, 
+									noData: yd?.value === null,
+									suppressed: yd?.suppressed,
+									moeLow: yd?.moeLow, 
+									moeHigh: yd?.moeHigh
+								};
+							}),
+						type: 'line',
+						stack: 'moe',
+						lineStyle: { opacity: 0 },
+						itemStyle: { opacity: 0 },
+						areaStyle: { opacity: 0.25 },
+						color: '#b8237e'
+					});
+				}
+			})
+
 			let maxValue = Math.max(...allValues);
 			let minValue = Math.min(0, ...allValues);
 			let rounder = 1;
@@ -280,6 +322,31 @@ export default {
 			option.yAxis.axisLabel.formatter = (value) => {
 				return value === axisMin || value === axisMax || value === 0 ? Number(value).toLocaleString() : '';
 			};
+
+			if (this.compareSelections) {
+				option.legend = {
+					top: 0,
+					left: 'center',
+					data: seriesNames,
+					selectedMode: false
+				};
+				option.tooltip = {
+					trigger: 'axis',
+					axisPointer: {
+						type: 'shadow'
+					},
+					textStyle: Object.assign({}, textStyle),
+					formatter: (params) => {
+						let rows = [params[0].name]
+						params.forEach(p => {
+							if (p.seriesType === 'line') {
+								rows.push(p.marker + p.seriesName + ': ' + format(this.exploreData.indicator.typeId, p.value))
+							}
+						});
+						return rows.join('<br/>');
+					}
+				};
+			}
 
 			this.chart.setOption(option);
 		}
